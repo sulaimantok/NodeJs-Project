124cecdc570e058acef028122278c11a86743a23
SULAIMAN <sulaimanniceguy@gmail.com>
2020-07-21 09:22:23 +0700

    Update content using view engine jade

--- Express/app.js
+++ Express/app.js
@@ -5,12 +5,16 @@ var nodemailer = require('nodemailer');
 
 var app = express();
 
+app.set('views',path.join(__dirname,"views"));
+app.set('view engine','jade');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/',function(req,res){
 	//console.log('Hello, World');
-	res.send('Hello,World')
+	//res.send('Hello,World')
+	res.render('index');
 });
 app.listen(3000);
 console.log('Server is running on port 3000...');
